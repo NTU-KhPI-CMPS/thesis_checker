edc94fa2a0f7ad40f705504a239d588f406272f9
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-03-11 21:36:32 +0200

    chore: added MultiBlocProvider

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/features/home/view/home_view.dart';
+import 'package:flutter_app/app_view.dart';
+import 'package:flutter_app/features/home/bloc/file_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView()
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => FileBloc()),
+      ],
+      child: const AppView(),
     );
   }
 }
