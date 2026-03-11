39f9a6decbb281ff816435d3ff77da14c5a7ea35
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-03-11 22:36:31 +0200

    chore: code prepared for further development

--- flutter_app/lib/app.dart
+++ flutter_app/lib/app.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/features/home/view/home_view.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView()
-    );
+    return const Placeholder();
   }
 }
