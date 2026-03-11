c4b8a2f4d525893f5c6578a2d14be45da2453c77
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-03-11 21:37:35 +0200

    chore: added bloc builder

--- lib/features/analysis/view/analysis_view.dart
+++ lib/features/analysis/view/analysis_view.dart
@@ -1,50 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/core/constants/app_colors.dart';
 import 'package:flutter_app/features/analysis/widgets/dual_ring_indicator.dart';
+import 'package:flutter_app/features/home/bloc/file_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AnalysisView extends StatelessWidget {
   const AnalysisView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: SizedBox(
-          width: 260.0,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              DualRingIndicator(),
-              SizedBox(height: 24.0),
-              Text(
-                'Аналізую документ...',
-                style: TextStyle(
-                  color: AppColors.text,
-                  fontFamily: 'FunnelSans',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w600,
+    return BlocBuilder<FileBloc, FileState>(
+      builder: (context, state) {
+        final fileName = state is FileLoadedState ? state.fileName : 'назва_документу.docx';
+        return Center(
+          child: SizedBox(
+            width: 260.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                DualRingIndicator(),
+                SizedBox(height: 24.0),
+                Text(
+                  'Аналізую документ...',
+                  style: TextStyle(
+                    color: AppColors.text,
+                    fontFamily: 'FunnelSans',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                'назва_документу.docx · n сторінок',
-                style: TextStyle(
-                  fontFamily: 'FunnelSans',
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.w600,
-                  color: AppColors.text2,
+                SizedBox(height: 8.0),
+                Text(
+                  '$fileName · n сторінок',
+                  style: TextStyle(
+                    fontFamily: 'FunnelSans',
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.text2,
+                  ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              LinearProgressIndicator(
-                borderRadius: BorderRadius.circular(4.0),
-                value: 0.8,
-                backgroundColor: AppColors.border,
-                valueColor: AlwaysStoppedAnimation<Color>(AppColors.accent),
-              ),
-            ],
+                SizedBox(height: 16.0),
+                LinearProgressIndicator(
+                  borderRadius: BorderRadius.circular(4.0),
+                  value: 0.8,
+                  backgroundColor: AppColors.border,
+                  valueColor: AlwaysStoppedAnimation<Color>(AppColors.accent),
+                ),
+              ],
+            ),
           ),
-        ),
-      );
+        );
+      },
+    );
   }
 }
