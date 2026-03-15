ab908092ae21205fe9b0624c1f57332e294a2ac6
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-03-15 13:47:02 +0200

    chore: moved the content container to a separate file

---
+++ lib/core/widgets/page_container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/core/constants/app_colors.dart';
+
+class PageContainer extends StatelessWidget {
+  final Widget child;
+  const PageContainer({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        color: AppColors.bg,
+        border: Border(
+          top: BorderSide(
+            color: AppColors.border,
+            width: 1.0,
+          ),
+        ),
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final minHeight = constraints.maxHeight;
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: minHeight,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Center(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxWidth: 820.0,
+                    ),
+                    child: child,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
