2ae1550c19a57e8f227189af98ccda2cf5f6de1b
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-04-05 21:24:14 +0300

    chore: refactoring

--- lib/core/models/info_item_model.dart
+++ lib/core/models/info_item_model.dart
@@ -1,18 +1,12 @@
-/// Reusable presentation model for UI items with icon and text blocks.
-///
-/// Can be used in info cards and any other compact list/grid UI sections.
-class InfoItemModel {
+/// Reusable presentation model for UI cards with icon and text blocks.
+class CheckTypeInfo {
   final String iconPath;
   final String title;
-  final String subtitle;
-  final String? description;
-  final Map<String, String>? meta;
+  final String description;
 
-  const InfoItemModel({
+  const CheckTypeInfo({
     required this.iconPath,
     required this.title,
-    required this.subtitle,
-    this.description,
-    this.meta,
+    required this.description,
   });
 }
