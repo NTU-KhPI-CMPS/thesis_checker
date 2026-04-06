8b1f7977523d850e4c32fb6b9f366b0e071a3aba
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-04-06 11:04:11 +0300

    chore: refactoring

--- lib/core/models/info_item_model.dart
+++ lib/core/models/info_item_model.dart
@@ -1,12 +1,12 @@
 /// UI model for cards describing available check types.
 class CheckTypeInfo {
-  final String iconPath;
   final String title;
+  final String iconPath;
   final String description;
 
   const CheckTypeInfo({
-    required this.iconPath,
     required this.title,
+    required this.iconPath,
     required this.description,
   });
 }
