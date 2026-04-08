fa1f3d22180fd3c901059a3f60aee208e016f322
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-04-08 18:54:52 +0300

    chore: added a class for saving images used in the application

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,18 @@
+class AppIcons {
+  // app logo icon
+  static const String appIcon = 'assets/images/page_facing_up.png';
+
+  // switch theme icons
+  static const String lightThemeIcon = 'assets/images/sunny.png';
+  static const String darkThemeIcon = 'assets/images/moon.png';
+
+  // menu icons
+  static const String homeIcon = 'assets/images/house.png';
+  static const String analysisIcon = 'assets/images/hourglass.png';
+
+  // upload zone icon
+  static const String uploadIcon = 'assets/images/open_folder.png';
+
+  // checks icons
+  static const String fontCheckIcon = 'assets/images/abc.png';
+}
