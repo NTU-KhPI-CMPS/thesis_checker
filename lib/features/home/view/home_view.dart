53a50b507b4b5722c24f7e43ca781174bf01e252
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-04-08 18:57:06 +0300

    chore: refactoring

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/constants/app_icons.dart';
 import 'package:flutter_app/core/theme/theme_cubit.dart';
 import 'package:flutter_app/core/widgets/page_container.dart';
 import 'package:flutter_app/features/home/bloc/file_bloc.dart';
@@ -22,8 +23,8 @@ class _HomeViewState extends State<HomeView> {
   bool themeButtonIsHovered = false;
 
   final List<Map<String, dynamic>> buttons = [
-    {'icon': 'assets/images/house.png', 'label': 'Головна'},
-    {'icon': 'assets/images/hourglass.png', 'label': 'Аналіз'},
+    {'icon': AppIcons.homeIcon, 'label': 'Головна'},
+    {'icon': AppIcons.analysisIcon, 'label': 'Аналіз'},
   ];
 
   void _onAnalysisComplete() {    
@@ -107,7 +108,7 @@ class _HomeViewState extends State<HomeView> {
                   child: Padding(
                     padding: const EdgeInsets.all(4.0),
                     child: Image.asset(
-                      'assets/images/page_facing_up.png',
+                      AppIcons.appIcon,
                       width: 20.0,
                       height: 20.0,
                     ),
@@ -150,8 +151,8 @@ class _HomeViewState extends State<HomeView> {
                     children: [
                       Image.asset(
                         isLight
-                            ? 'assets/images/sunny.png'
-                            : 'assets/images/moon.png',
+                            ? AppIcons.lightThemeIcon
+                            : AppIcons.darkThemeIcon,
                         width: 20.0,
                         height: 20.0,
                       ),
