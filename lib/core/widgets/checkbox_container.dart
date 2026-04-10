784037714643c62a54068c3e0d3673e6c2fa2e9f
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-04-10 12:35:06 +0300

    chore: error indicator added

--- lib/core/widgets/checkbox_container.dart
+++ lib/core/widgets/checkbox_container.dart
@@ -7,6 +7,8 @@ class CheckboxContainer extends StatefulWidget {
   final VoidCallback onTap;
   final bool isSelected;
   final double borderWidth;
+  final Color? bottomStripeColor;
+  final double indicatorHeight;
 
   const CheckboxContainer({
     super.key, 
@@ -15,6 +17,8 @@ class CheckboxContainer extends StatefulWidget {
     required this.rightWidget,
     this.isSelected = false,
     this.borderWidth = 1.0,
+    this.bottomStripeColor,
+    this.indicatorHeight = 4.0,
   });
 
   @override
@@ -44,19 +48,31 @@ class _CheckboxContainerState extends State<CheckboxContainer> {
           padding: const EdgeInsets.all(12.0),
           decoration: BoxDecoration(
             color: isActive ? activeBackgroundColor : backgroundColor,
-            border: Border.all(color: isHovered || isActive ? hoverBorderColor : borderColor!, width: widget.borderWidth),
+            border: Border.all(
+              color: isHovered || isActive ? hoverBorderColor : borderColor!, 
+              width: widget.borderWidth
+            ),
             borderRadius: BorderRadius.circular(12.0),
           ),
           child: Stack(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 30.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: widget.children,
-                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: widget.children,
               ),
+              if (widget.bottomStripeColor != null)
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    height: widget.indicatorHeight,
+                    decoration: BoxDecoration(
+                      color: widget.bottomStripeColor,
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                  ),
+                ),
               Positioned(
                 top: 0,
                 right: 0,
