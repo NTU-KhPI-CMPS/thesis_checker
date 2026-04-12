0c7a4d5c1e8c9a0b9eb454979f8788e7b208c6ef
HappyMary16 <m.borodin.1999@gmail.com>
2026-04-12 16:41:35 +0200

    Generate and sign msix installer

---
+++ tools/sign.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set installer=build\windows\x64\runner\Release\flutter_app.msix
+
+call signtool sign /v /debug /fd SHA256 /tr http://timestamp.acs.microsoft.com /td SHA256 ^
+    /dlib "C:\Program Files\signtool\Azure.CodeSigning.Dlib.dll" /dmdf tools\signing.json %installer%
