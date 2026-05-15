4a1575524884254de869c4a369af4a3bccfa91a8
mborodin <m.borodin.1999@gmail.com>
2026-05-15 22:04:00 +0200

    TC-111 add new check types

--- lib/core/constants/available_check_types.dart
+++ lib/core/constants/available_check_types.dart
@@ -5,9 +5,21 @@ class AvailableCheckTypes {
   static const List<CheckTypeInfo> checkTypes = [
     CheckTypeInfo(
       title: 'Шрифт',
-      description: 'Назва, розмір',
+      description: 'Назва і розмір шрифту',
       checks: [Check.fontName, Check.fontSize],
       iconPath: 'assets/images/abc.png',
+    ),
+    CheckTypeInfo(
+      title: 'Інтервали',
+      description: 'Міжрядковий і відступи навколо абзаців',
+      checks: [Check.lineSpacing, Check.spacing],
+      iconPath: 'assets/images/up_down_arrow.png',
+    ),
+    CheckTypeInfo(
+      title: 'Вирівнювання',
+      description: 'Вирівнювання абзаців і заголовків',
+      checks: [Check.alignment],
+      iconPath: 'assets/images/left_right_arrow.png',
     ),
     CheckTypeInfo(
       title: 'Інші',
--- lib/core/enums/check.dart
+++ lib/core/enums/check.dart
@@ -1,7 +1,12 @@
 /// Central enum with all available thesis checks.
 enum Check {
   fontName('FONT_NAME'),
-  fontSize('FONT_SIZE');
+  fontSize('FONT_SIZE'),
+  lineSpacing('LINE_SPACING'),
+  spacing('SPACING'),
+  alignment('ALIGNMENT'),
+  indentation('INDENTATION'),
+  firstLineIndentation('FIRST_LINE_INDENTATION');
 
   final String name;
   const Check(this.name);
