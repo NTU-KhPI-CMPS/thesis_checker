1a41850d427ddba871c6ad66725926a9bad917d7
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-07-23 13:45:46 +0300

    TC-149: added other check codes for fix re-run checks

--- thesis_checker_app/lib/core/constants/available_check_types.dart
+++ thesis_checker_app/lib/core/constants/available_check_types.dart
@@ -1,18 +1,25 @@
 import 'package:thesis_checker/core/enums/check.dart';
 import 'package:thesis_checker/models/check_type_info.dart';
 
+/// Class that holds the available check types for document analysis.
 class AvailableCheckTypes {
   static const List<CheckTypeInfo> checkTypes = [
     CheckTypeInfo(
       title: 'Шрифт',
       description: 'Назва і розмір шрифту',
-      checks: [Check.fontName, Check.fontSize],
+      checks: [
+        Check.fontName,
+        Check.fontSize
+      ],
       iconPath: 'assets/images/abc.png',
     ),
     CheckTypeInfo(
       title: 'Інтервали',
       description: 'Міжрядковий і відступи навколо абзаців',
-      checks: [Check.lineSpacing, Check.spacing],
+      checks: [
+        Check.lineSpacing,
+        Check.spacing
+      ],
       iconPath: 'assets/images/up_down_arrow.png',
     ),
     CheckTypeInfo(
@@ -24,7 +31,12 @@ class AvailableCheckTypes {
     CheckTypeInfo(
       title: 'Інші',
       description: 'Інші перевірки, які не входять до категорій',
-      checks: [],
+      checks: [
+        Check.listFormatting,
+        Check.structuralElement,
+        Check.formula,
+        Check.indentation
+      ],
       iconPath: 'assets/images/other_checks.png',
     )
   ];
--- thesis_checker_app/lib/core/enums/check.dart
+++ thesis_checker_app/lib/core/enums/check.dart
@@ -3,10 +3,13 @@ enum Check {
   fontName('FONT_NAME'),
   fontSize('FONT_SIZE'),
   lineSpacing('LINE_SPACING'),
-  spacing('SPACING'),
   alignment('ALIGNMENT'),
+  listFormatting('LIST_FORMATTING'),
+  spacing('SPACING'),
   indentation('INDENTATION'),
-  firstLineIndentation('FIRST_LINE_INDENTATION');
+  // firstLineIndentation('FIRST_LINE_INDENTATION'), need to add this code when the checker will be implemented
+  structuralElement('STRUCTURAL_ELEMENT'),
+  formula('FORMULA');
 
   final String name;
   const Check(this.name);
